3856659d3e9286584a3d601dc63bb645c6f5c4b9
hpaugam <hugo.paugam@gmail.com>
2019-01-31 18:01:16 +0100

    First version of continents rising

---
+++ Assets/Scripts/EarthRising.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EarthRising : MonoBehaviour
+{
+    public Camera cam;
+    Mesh m;
+    Vector3[] vertices;
+
+    void Start()
+    {
+         m = GetComponent<MeshFilter>().mesh;
+        vertices = m.vertices;
+        
+    }
+
+    void Update()
+    {
+        //cam = GetComponent<Camera>();
+
+        if (!Input.GetMouseButton(0))
+            return;
+
+        RaycastHit hit;
+        if (!Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hit))
+            return;
+
+        Renderer rend = hit.transform.GetComponent<Renderer>();
+        MeshCollider meshCollider = hit.collider as MeshCollider;
+
+        //Debug.Log(hit.collider.gameObject.name);
+
+        /*if (rend == null || rend.sharedMaterial == null || rend.sharedMaterial.mainTexture == null || meshCollider == null)
+            return;*/
+
+       
+
+        Vector3 pointSphere = transform.InverseTransformPoint(hit.point);
+        float distMin = Vector3.Distance(vertices[0], pointSphere);
+        int indexMin = 0;
+
+        for (int i = 1; i < vertices.Length; i++)
+        {
+            if (Vector3.Distance(vertices[i], pointSphere) < distMin)
+            {
+                //Debug.Log(vertices[i].x);
+                distMin = Vector3.Distance(vertices[i], pointSphere);
+                indexMin = i;
+            }
+        }
+        vertices[indexMin] += m.normals[indexMin] / 1000;
+
+        Debug.Log(vertices[indexMin].x);
+        // Debug.Log(hit.collider.gameObject.name);
+        //  Debug.Log(pointSphere.x);
+
+
+
+        //Vector3 pointSphereNormal = 
+
+        /*
+         m.vertices;
+         m.normals;
+         */
+        m.vertices = vertices;
+        m.RecalculateBounds();
+    }
+}
